ea2c077ffd864d2b1c859c9d9736e74ec88f2204
cjkook <cjko.audio@gmail.com>
2020-06-29 09:49:42 -0600

    intial script for quiz added

---
+++ js/Code-Quiz-js/code-quiz.js
@@ -0,0 +1,124 @@
+// update chart from stored data
+
+// initialize all fields
+function fnPageSetup() {
+
+}
+
+// start quiz
+function fnStart() {
+
+}
+
+const questions = [
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+    {
+        title: "",
+        answers: [
+            ""
+        ],
+        correct: 0,
+        qText: "",
+        category: [
+            ""
+        ]
+    },
+]
